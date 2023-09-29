4b71cc2f95478acf769f320cf1aa0d0ba561bfd2
İsmail Çakıcı <ismailcakici30@gmail.com>
2023-09-29 23:18:20 +0300

    h1 first vers created

--- src/components/headers/h1.jsx
+++ src/components/headers/h1.jsx
@@ -1,8 +1,23 @@
 import React from "react";
-import { logo } from "../../assets/icons/logo-sm.png";
+import logo from "../../assets/icons/logo-sm.png";
 
 const H1 = () => {
-  return <div className="bg-white w-full h-16 p-1 flex flex-row shadow-xl"></div>;
+  return (
+    <div className="bg-white w-full h-16 p-1 flex flex-row items-center justify-between shadow-xl px-16">
+      <div className="min-w-min flex justify-center items-center gap-4">
+        <img className="w-12 h-12 grayscale cursor-pointer" src={logo} alt="ic-logo" />
+        <p className="text-grey-0 font-semibold text-lg">LOGO</p>
+      </div>
+      <div className="min-w-min">
+        <ul className="flex flex-row justify-around items-center font-semibold gap-3">
+          <li className="cursor-pointer">Home</li>
+          <li className="cursor-pointer">About Us</li>
+          <li className="cursor-pointer">Contact</li>
+          <li className="cursor-pointer">F.A.Q</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default H1;
