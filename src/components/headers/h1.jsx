cedaa9f9eb237c6a983aa2257cce71f1399df7b6
İsmail Çakıcı <ismailcakici30@gmail.com>
2023-09-29 22:41:52 +0300

    h1 updated

--- src/components/headers/h1.jsx
+++ src/components/headers/h1.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { logo } from "../../assets/icons/logo-sm.png";
 
 const H1 = () => {
-  return <div>H1 GELDİ</div>;
+  return <div className="bg-white w-full h-16 p-1 flex flex-row shadow-xl"></div>;
 };
 
 export default H1;
