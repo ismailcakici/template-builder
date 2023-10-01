093da6dd86c113672bf9f4eb5b6418aecb6f84b1
İsmail Çakıcı <ismailcakici30@gmail.com>
2023-10-01 17:11:38 +0300

    header 2 completed

--- src/components/headers/h2.jsx
+++ src/components/headers/h2.jsx
@@ -11,11 +11,9 @@ const H2 = () => {
   const [menuActive, setMenuActive] = useState(false);
 
   // handle menu state
-
   const handleSetMenuActive = () => setMenuActive(!menuActive);
 
   // handle menu state when port changes
-
   useEffect(() => {
     selectedPort !== "mobile" && setMenuActive(false);
   }, [selectedPort]);
@@ -28,62 +26,70 @@ const H2 = () => {
           ? "justify-between"
           : selectedPort === "mobile"
           ? "justify-start px-4"
-          : "justify-center"
+          : "justify-evenly"
       }`}
     >
-      {/* menu section */}
-      <div
-        className={`flex flex-col gap-1 cursor-pointer ${
-          selectedPort === "mobile" ? "block" : "hidden"
-        }`}
-        onClick={handleSetMenuActive}
-      >
-        <div
-          className={`w-8 h-1 rounded-lg bg-grey-0 transition-all ${
-            menuActive && "rotate-45 -mb-2"
-          }`}
-        ></div>
-        <div
-          className={`w-8 h-1 rounded-lg bg-grey-0 transition-all ${
-            menuActive && "-translate-x-5 opacity-0"
-          }`}
-        ></div>
-        <div
-          className={`w-8 h-1 rounded-lg bg-grey-0 transition-all ${
-            menuActive && "-rotate-45 -mt-2"
-          }`}
-        ></div>
-      </div>
-      {/* logo section */}
-      <div
-        className={`min-w-min flex justify-center items-center gap-4 ${
-          selectedPort === "tablet" ? "hidden" : selectedPort === "mobile" ? "hidden" : "block"
-        }`}
-      >
+      {/* Left Section */}
+      {selectedPort !== "mobile" && (
+        <div className="flex items-center gap-4 font-semibold">
+          {headerConstants.slice(0, 3).map((title, idx) => (
+            <div className="cursor-pointer hover:text-grey-50" key={idx}>
+              {title}
+            </div>
+          ))}
+        </div>
+      )}
+
+      {/* Middle Section (Logo) */}
+      <div className={`flex items-center gap-4 ${selectedPort === "mobile" && "hidden"}`}>
         <img className="w-12 h-12 grayscale cursor-pointer" src={logo} alt="ic-logo" />
         <p className="text-grey-0 font-semibold text-lg">LOGO</p>
       </div>
-      {/* navbar section */}
-      <div
-        className={`${selectedPort === "mobile" ? !menuActive && "hidden" : "block"} ${
-          menuActive && "relative"
-        }`}
-      >
-        <ul
-          className={`flex flex-row justify-around items-center font-semibold gap-3 ${
-            menuActive &&
-            "absolute left-[-47px] top-8 px-20 py-40 gap-12 flex-col text-center bg-white"
-          }`}
-        >
-          {headerConstants.map((title, idx) => {
-            return (
+
+      {/* Right Section */}
+      {selectedPort !== "mobile" && (
+        <div className="flex items-center gap-4 font-semibold">
+          {headerConstants.slice(3).map((title, idx) => (
+            <div className="cursor-pointer hover:text-grey-50" key={idx}>
+              {title}
+            </div>
+          ))}
+        </div>
+      )}
+
+      {/* Mobile Menu Icon */}
+      {selectedPort === "mobile" && (
+        <div className={`flex flex-col gap-1 cursor-pointer`} onClick={handleSetMenuActive}>
+          <div
+            className={`w-8 h-1 rounded-lg bg-grey-0 transition-all ${
+              menuActive && "rotate-45 -mb-2"
+            }`}
+          ></div>
+          <div
+            className={`w-8 h-1 rounded-lg bg-grey-0 transition-all ${
+              menuActive && "-translate-x-5 opacity-0"
+            }`}
+          ></div>
+          <div
+            className={`w-8 h-1 rounded-lg bg-grey-0 transition-all ${
+              menuActive && "-rotate-45 -mt-2"
+            }`}
+          ></div>
+        </div>
+      )}
+
+      {/* Mobile Menu */}
+      {selectedPort === "mobile" && menuActive && (
+        <div className="relative">
+          <ul className="absolute left-[-47px] top-8 px-20 py-40 gap-12 flex flex-col text-center bg-white font-semibold">
+            {headerConstants.map((title, idx) => (
               <li className="cursor-pointer hover:text-grey-50" key={idx}>
                 {title}
               </li>
-            );
-          })}
-        </ul>
-      </div>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
