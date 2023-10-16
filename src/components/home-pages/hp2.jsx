62b400b7d7da8e516556cdcec399525cf7bfafb2
İsmail Çakıcı <ismailcakici30@gmail.com>
2023-10-16 20:02:49 +0300

    all home pages finished

--- src/components/home-pages/hp2.jsx
+++ src/components/home-pages/hp2.jsx
@@ -19,25 +19,34 @@ const HP2 = () => {
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center">
         <span
           className={`text-white-10 text-center font-bold ${
-            selectedPort === "mobile" ? "text-4xl" : "text-6xl "
+            selectedPort === "mobile" ? "text-5xl" : "text-6xl "
           }`}
         >
           Your Title Here
         </span>
         <span
           className={`text-white text-center  font-semibold my-2 ${
-            selectedPort === "mobile" ? "text-2xl" : "text-4xl"
+            selectedPort === "mobile" ? "text-xl" : "text-4xl"
           }`}
         >
           Your Slogan Here
         </span>
-        <button
-          className={`self-center  bg-white-10 shadow-lg rounded-md my-5 font-semibold transition-all hover:bg-grey-0 hover:text-white-10 ${
-            selectedPort === "mobile" ? "w-32 h-12" : "w-36 h-16"
-          }`}
-        >
-          Explore
-        </button>
+        <div className="flex flex-row justify-around w-full gap-2">
+          <button
+            className={`self-center  bg-white-10 shadow-lg rounded-md my-5 font-semibold transition-all hover:bg-grey-0 hover:text-white-10 ${
+              selectedPort === "mobile" ? "w-32 h-12" : "w-36 h-16"
+            }`}
+          >
+            Explore
+          </button>
+          <button
+            className={`self-center  bg-white-10 shadow-lg rounded-md my-5 font-semibold transition-all hover:bg-grey-0 hover:text-white-10 ${
+              selectedPort === "mobile" ? "w-32 h-12" : "w-36 h-16"
+            }`}
+          >
+            See More
+          </button>
+        </div>
       </div>
     </div>
   );
